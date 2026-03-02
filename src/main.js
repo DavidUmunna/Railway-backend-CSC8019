798b338cb299dbfc8f7701414e1a61a5df7834e2
chima1968 <chimarokeumunna98@gmail.com>
2026-03-02 17:17:49 +0100

    Remove  files

--- src/main.js
+++
@@ -1,8 +0,0 @@
-// Entry file of the Vue application
-// This file mounts the root component (App.vue) to the DOM
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-// Create Vue app instance and mount it
-createApp(App).mount('#app')
