b860224ca5522278ea7f179e6d79ed321c657d34
chima1968 <chimarokeumunna98@gmail.com>
2026-04-22 11:39:19 +0100

    refactor: change integer types to bigint for primary and foreign keys in SQL tables

--- Backend/sql scripts/coffee_shop.sql
+++ Backend/sql scripts/coffee_shop.sql
@@ -1,12 +1,12 @@
 CREATE TABLE customer (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     customer_firstname VARCHAR(50) NOT NULL,
     customer_lastname VARCHAR(50) NOT NULL,
     customer_phone_number VARCHAR(14)
 );
 
 CREATE TABLE station(
-    station_id INT PRIMARY KEY,
+    station_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     station_name VARCHAR(100),
     weekday_opening_hours VARCHAR(55),
     saturday_opening_hours VARCHAR(55),
@@ -14,7 +14,7 @@ CREATE TABLE station(
 );
 
 CREATE TABLE staff (
-    staff_id INT AUTO_INCREMENT PRIMARY KEY,
+    staff_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     staff_name VARCHAR(100),
     staff_role CHAR(150),
     username CHAR(150),
@@ -22,9 +22,9 @@ CREATE TABLE staff (
 );
 
 CREATE TABLE purchase_order (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT,
-    staff_id INT,
+    order_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    customer_id BIGINT,
+    staff_id BIGINT,
     order_date DATE NOT NULL,
     pickup_time TIME NOT NULL,
     order_status ENUM('ACCEPTED','IN_PROGRESS','COMPLETED', 'COLLECTED', 'CANCELLED') NOT NULL,
@@ -36,15 +36,15 @@ CREATE TABLE purchase_order (
 );
 
 CREATE TABLE menu_item (
-    menu_item_id INT AUTO_INCREMENT PRIMARY KEY,
+    menu_item_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     item_name VARCHAR(150) NOT NULL,
     item_description VARCHAR(300),
     is_available BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE menu_item_type(
-    menu_item_type_id INT AUTO_INCREMENT PRIMARY KEY,
-    menu_item_id INT,
+    menu_item_type_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    menu_item_id BIGINT,
     size_name ENUM('Regular','Large'),
     price DECIMAL(8,2) DEFAULT 0.00 NOT NULL,
     is_available BOOLEAN DEFAULT TRUE,
@@ -52,8 +52,8 @@ CREATE TABLE menu_item_type(
 );
 
 CREATE TABLE order_item (
-    purchase_order_id INT,
-    menu_item_type_id INT,
+    purchase_order_id BIGINT,
+    menu_item_type_id BIGINT,
     quantity INT,
     unit_price DECIMAL(8,2) DEFAULT 0.00 NOT NULL,
     line_total DECIMAL(8,2) DEFAULT 0.00 NOT NULL,
@@ -63,7 +63,7 @@ CREATE TABLE order_item (
 );
 
 CREATE TABLE order_staff_relation (
-    order_id INT,
+    order_id BIGINT,
     staff_id INT,
     PRIMARY KEY (order_id, staff_id),
     FOREIGN KEY (order_id) REFERENCES purchase_order(order_id) ON DELETE CASCADE,
